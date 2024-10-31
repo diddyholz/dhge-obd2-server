34a30f379b1da60cfa318f95365367bf8ee7fa56
Sidney Krombholz <sidney.krombholz@gmail.com>
2024-10-31 19:05:59 +0100

    Print stderr when command to change bitrate failed

--- src/server/obd2_bridge/obd2_bridge_linux.cpp
+++ src/server/obd2_bridge/obd2_bridge_linux.cpp
@@ -1,15 +1,18 @@
 #include "obd2_bridge.h"
 
 #include <iostream>
+#include <fstream>
 
 namespace obd2_server {
+    int32_t execute_command(std::string command, std::string &output);
+
     void obd2_bridge::setup_can_device() {
         if (skip_can_setup) {
             return;
         }
 
-    // Before setting up can device, shut it down first, to change attributes
-    shutdown_can_device();
+        // Before setting up can device, shut it down first, to change attributes
+        shutdown_can_device();
 
         // Make sure the can device string is safe to use as a shell parameter
         for (char c : can_device) {
@@ -19,29 +22,51 @@ namespace obd2_server {
         }
 
         std::string command = "ip link set " + can_device + " up type can bitrate " 
-            + std::to_string(can_bitrate) + " loopback off > /dev/null 2> /dev/null";
+            + std::to_string(can_bitrate) + " loopback off";
+        std::string output;
 
-        if (system(command.c_str()) != 0) {
-            throw std::runtime_error("Could not set up CAN device: Command failed, please retry as root");
+        if (execute_command(command, output) != 0) {
+            throw std::runtime_error("Could not set up CAN device: " + output);
         }
     }
 
     void obd2_bridge::shutdown_can_device() {
         if (skip_can_setup) {
-        return;
+            return;
+        }
+
+        // Make sure the can device string is safe to use as a shell parameter
+        for (char c : can_device) {
+            if (!isalnum(c)) {
+                    throw std::invalid_argument("Could not shutdown CAN device: Invalid device name");
+                }
+        }
+
+        std::string command = "ip link set " + can_device + "down";
+        std::string output;
+
+        if (execute_command(command, output) != 0) {
+            throw std::runtime_error("Could not shut down CAN device: " + output);
+        }
     }
 
-    // Make sure the can device string is safe to use as a shell parameter
-    for (char c : can_device) {
-        if (!isalnum(c)) {
-                throw std::invalid_argument("Could not shutdown CAN device: Invalid device name");
-            }
-    }
+    int32_t execute_command(std::string command, std::string &output) {
+        uint32_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
 
-    std::string command = "ip link set " + can_device + " down";
+        std::string tmp_file = std::filesystem::temp_directory_path().string() 
+            + "/obd2_tmp_" + std::to_string(timestamp);
 
-    if (system(command.c_str()) != 0) {
-        throw std::runtime_error("Could not shut down CAN device: Command failed, please retry as root");
-    }
+        command += " > " + tmp_file + " 2>&1";
+
+        int32_t ret = system(command.c_str());
+
+        std::ifstream file(tmp_file);
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+
+        output = buffer.str();
+
+        return ret;
     }
 }
